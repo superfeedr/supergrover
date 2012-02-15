08778727ba86a45d769886cf6a455100d5e1e435
julien <julien.genestoux@gmail.com>
2012-02-14 16:45:04 -0800

    more changes yes that's a bad commit message

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ var app = module.exports = express.createServer();
 
 // Configuration
 
-// var baseUrl = 'http://severe-ice-3735.herokuapp.com';
-var baseUrl = 'https://plant-leg.showoff.io';
+var baseUrl = 'http://severe-ice-3735.herokuapp.com';
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -66,11 +65,12 @@ app.get('/', function(req, res){
 // Update method
 app.post('/', function(req, res) {
     var groveio = req.param('groveio')
-        , feed = req.param('feed');
+        , feed = req.param('feed')
+        , mode = req.param('mode');
     
     // The params for the request
     var postData = querystring.stringify({
-        'hub.mode' : 'subscribe',
+        'hub.mode' : mode,
         'hub.topic': feed,
         'hub.callback': baseUrl + '/superfeedr/' + (new Buffer(groveio).toString('base64')) + '/' + (new Buffer(feed).toString('base64')) ,
     });
@@ -78,9 +78,43 @@ app.post('/', function(req, res) {
     superfeedrOptions.headers['Content-Length'] = postData.length
     
     // Let's make a request to Superfeedr.
-    var req = https.request(superfeedrOptions, function(response) {
+    var req = http.request(superfeedrOptions, function(response) {
       response.setEncoding('utf8');
       if(response.statusCode === 204) {
+
+          var postData = {
+              'service' : 'Supergrover',
+              'url': baseUrl + '/?groveio=' + (new Buffer(groveio).toString('base64')) + '&feed=' + (new Buffer(feed).toString('base64')),
+              'icon_url': "https://grove.io/static/img/avatar.png"
+          }
+          
+          if(mode === "subscribe") {
+              postData.message = "This channel is now following " + feed + ". Be ready!";
+          }
+          else if(mode === "unsubscribe") {
+              postData.message = "This channel is not following " + feed + " anymore.";
+          }
+          postData = querystring.stringify(postData);
+          
+          var uri = url.parse(groveio);
+          groveioOptions.host =  uri.hostname;
+          groveioOptions.port =  uri.port;
+          groveioOptions.path =  uri.pathname;
+          groveioOptions.headers['Content-Length'] = postData.length
+
+          var req = https.request(groveioOptions, function(response) {
+              response.on('data', function(chunk) {
+                  console.log("Grove.io says: " + chunk);
+              });
+          });
+
+          req.on('error', function(e) {
+              console.log('problem with request: ' + e.message);
+          });
+
+          // write data to request body
+          req.write(postData);
+          req.end();
           res.render('success', { title: 'Supergroover', groveio: groveio, feed: feed});
       }
       else {
@@ -90,7 +124,7 @@ app.post('/', function(req, res) {
 
     req.on('error', function(e) {
       console.log('problem with request: ' + e.message);
-      res.render('index', { title: 'Supergroover', groveio: groveio, feed: feed });
+      res.render('failure', { title: 'Supergroover', groveio: groveio, feed: feed });
     });
     
     // write data to request body
@@ -126,7 +160,6 @@ app.post('/superfeedr/:groveio64/:feed64', function(req, res) {
         groveioOptions.port =  uri.port;
         groveioOptions.path =  uri.pathname;
 
-        // Let's make a request to Superfeedr.
         var req = https.request(groveioOptions, function(response) {
             response.on('data', function(chunk) {
                 console.log("Grove.io says: " + chunk);
